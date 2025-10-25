da24d6a6232b9599074b71ec52ef7666bb9d80ab
Akshay Venugopal <akshayvenugopal007@gmail.com>
2025-10-25 14:28:09 +0530

    Changes to fix and add more conds

--- javascript/custom_localstorage.js
+++ javascript/custom_localstorage.js
@@ -5,16 +5,20 @@
 
 const localStorage = {
     cache: new Map(), // Changed from Set to Map
-    setItem: function(key, value) {
-        return this.cache.set(key, value); // Added 'this.'
+    setItem: function (key, value) {
+        if (typeof key === "string" && value) {
+            return this.cache.set(key, value); // Added 'this.'
+        } else {
+            return null; // Added return null for invalid cases
+        }
     },
-    getItem: function(key) {
+    getItem: function (key) {
         return this.cache.get(key); // Added 'return'
     },
-    removeItem: function(key) {
+    removeItem: function (key) {
         return this.cache.delete(key);
     },
-    clear: function() {
+    clear: function () {
         return this.cache.clear();
     }
 };
@@ -27,3 +31,36 @@ console.log(localStorage.removeItem("a"))
 console.log(localStorage.getItem("a"))
 console.log(localStorage.getItem("1"))
 console.log(localStorage.clear())
+
+localStorage.clear();
+localStorage.setItem('1', 'one');
+const answer1 = localStorage.getItem('1');
+
+localStorage.clear();
+localStorage.setItem('1', 'one');
+const answer2 = localStorage.getItem('1');
+
+localStorage.clear();
+localStorage.setItem('1', 'one');
+localStorage.removeItem('1');
+const answer3 = localStorage.getItem('1');
+
+localStorage.clear();
+localStorage.setItem('1', 'one');
+localStorage.setItem('2', 'one');
+localStorage.setItem('3', 'one');
+localStorage.setItem('4', 'one');
+localStorage.clear();
+const getOne = localStorage.getItem('1');
+console.log(getOne); // Expected output: 'one'
+
+localStorage.clear();
+localStorage.setItem('1', 'one');
+localStorage.setItem('2', 'one');
+localStorage.setItem('3', 'one');
+localStorage.setItem('4', 'one');
+localStorage.removeItem('2');
+localStorage.removeItem('3');
+
+localStorage.clear();
+localStorage.setItem();
